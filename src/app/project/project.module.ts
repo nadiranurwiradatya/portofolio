c95d4eae0b5b030f6a6a6360baeb5ff13f6d0ab0
nadiranurwiradatya <picdira@gmail.com>
2024-05-23 10:06:51 +0700

    Portofolio

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectComponent } from './project.component';
 import { SidebarModule } from '../sidebar/sidebar.module';
-import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 
 @NgModule({
   declarations: [ProjectComponent],
