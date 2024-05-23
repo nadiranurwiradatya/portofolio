875d5ff8379b24210bd5608d67a774ab5e622353
nadiranurwiradatya <picdira@gmail.com>
2024-05-23 10:33:20 +0700

    Portofolio

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  HostListener,
+  PLATFORM_ID,
+  Inject,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { ProjectDataService } from '../service/project.data.service';
 import { DetailComponent } from '../detail/detail.component';
 import { MatDialog } from '@angular/material/dialog';
-import AOS from 'aos';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-project',
@@ -18,16 +25,21 @@ export class ProjectComponent implements OnInit {
     private router: Router,
     private projectDataService: ProjectDataService,
     public dialog: MatDialog,
-    private elRef: ElementRef
+    private elRef: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
   ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Akses ke `document` hanya terjadi jika di browser
+      const element = document.querySelector('...'); // Sesuaikan dengan kebutuhanmu
+      // Operasi lainnya dengan `document`
+    }
+
+    // Subscribing to project data
     this.projectDataService.allproject$.subscribe((project) => {
       this.projectList = project;
     });
-
-    // Inisialisasi AOS
-    AOS.init();
   }
 
   // Method untuk membuka dialog
