56ae09959bd02a6b9f6ae861859bfd7f21039e21
nadiranurwiradatya <picdira@gmail.com>
2024-06-24 14:18:06 +0700

    Ganti Foto Dialog

--- src/app/service/project.data.service.ts
+++ src/app/service/project.data.service.ts
@@ -13,6 +13,7 @@ export class ProjectDataService {
       desc: 'ini adalah project pertama saya pada dunia UI/Ui yang bertemakan website yang menyediakan resep . ini adalah tugas akhir dari mata kuliah desain pengalaman pengguna , yang pada hasil akhirnya mendapatkan nilai A',
       imageUrl: '../assets/img/resepedia.png',
       color: '	#BE3B20',
+      tools: 'Figma, Canva, Waze',
     },
     {
       type: 'Social Media Specialist',
@@ -20,24 +21,29 @@ export class ProjectDataService {
       desc: 'Saya di pilih menjadi koordinator PDD dan bertugas untuk menyampaikan informasi melalui instagram @positifcup dan mengkoordinasi anggota yang bertugas sebagai dokumenter',
       color: '#0F8135',
       imageUrl: '../assets/img/positifcup.png',
+      tools: 'Photoshop, Canva,',
     },
     {
       type: 'Angular Typescript',
       title: 'Onic E sport',
       desc: 'ini adalah projek saya semasa magang di Zetta bytes company , ini adalah website admin untuk menambahkan roaster baru dan biodata roaster, saya menerapkan array of object , form array , reactive form , Service Injection , Responsive Website, Search on Component , dan component component lain . ',
       imageUrl: '../assets/img/onic.png',
+      tools:
+        'Visual Studio Code; Service Injection, Responsive Design, CRUD, Array Of Object, Array On Array Form, Translate',
     },
     {
       type: 'Social Media Specialist',
       title: 'PKKBN SI 2023',
       desc: 'Saya sebagai koordinator PDD yaitu untu mengelola dan menyampaikan infromasi melalui instagram PPKBNSI 2023 dan juga sebagai Dokumenter saat hari h',
       imageUrl: '../assets/img/pkkbn.png',
+      tools: 'Photoshop, Canva,',
     },
     {
       type: 'Social Media Specialist',
       title: 'Himasisfo',
       desc: 'Jobdesc pada saat menjabat sebagai wakil kepala departemen yaitu mengelola , menyampaikan informasi melalui instagram himasisfo , dan juga project pengenalan pengurus kabinet ',
       imageUrl: '../assets/img/himasisfo.png',
+      tools: 'Photoshop, Canva; 3D Design, Photography ',
     },
   ];
 
